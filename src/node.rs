126c01d0d7aec4809cb2c4b4cb9a231a36c22fd3
Ian <iwburns8@gmail.com>
2016-09-13 16:42:52 -0500

    add more methods to Node and add tests for them.

--- src/node.rs
+++ src/node.rs
@@ -22,6 +22,26 @@ impl<T> Node<T> {
     pub fn data_mut(&mut self) -> &mut T {
         &mut self.data
     }
+
+    pub fn parent(&self) -> Option<NodeId> {
+        self.parent
+    }
+
+    pub fn children(&self) -> &Vec<NodeId> {
+        &self.children
+    }
+
+    fn set_parent(&mut self, parent: NodeId) {
+        self.parent = Some(parent);
+    }
+
+    fn add_child(&mut self, child: NodeId) {
+        self.children.push(child);
+    }
+
+    fn children_mut(&mut self) -> &mut Vec<NodeId> {
+        &mut self.children
+    }
 }
 
 #[cfg(test)]
@@ -45,5 +65,39 @@ mod tests {
         let mut five = 5;
         let mut node = Node::new(five);
         assert_eq!(node.data_mut(), &mut five);
+    }
+
+    #[test]
+    fn test_parent() {
+        let node = Node::new(5);
+        assert_eq!(node.parent(), None);
+
+        let parent_id: NodeId = 0;
+        let mut node = Node::new(5);
+        node.set_parent(parent_id);
+
+        assert_eq!(node.parent(), Some(parent_id))
+    }
+
+    #[test]
+    fn test_children() {
+        let node = Node::new(5);
+        assert_eq!(node.children().len(), 0);
+
+        let mut node = Node::new(5);
+        let child_id: NodeId = 2;
+        node.add_child(child_id);
+        let children = node.children();
+
+        assert_eq!(children.len(), 1);
+        assert_eq!(children.get(0).unwrap(), &child_id);
+
+        let mut node = Node::new(5);
+        let child_id: NodeId = 2;
+        node.children_mut().push(child_id);
+        let children = node.children();
+
+        assert_eq!(children.len(), 1);
+        assert_eq!(children.get(0).unwrap(), &child_id);
     }
 }
