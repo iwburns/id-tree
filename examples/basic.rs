4de0da3e2cdbf937e236ede32dba57ff9a2b4dce
Ian Burns <iwburns8@gmail.com>
2016-09-20 09:23:19 -0500

    clean up the basic example a bit
    clean up the post order print out and add a preorder printout.

--- examples/basic.rs
+++ examples/basic.rs
@@ -12,25 +12,38 @@ fn main() {
 
     let root_node = Node::new(1);
     let child_1 = Node::new(2);
-    let child_2 = Node::new(3);
+    let child_3 = Node::new(3);
     let grandchild_4 = Node::new(4);
     let grandchild_5 = Node::new(5);
 
     let root_id: NodeId = tree.set_root(root_node);
     let child_1_id: NodeId = tree.add_child(root_id, child_1);
-    tree.add_child(root_id, child_2);
+    tree.add_child(root_id, child_3);
     tree.add_child(child_1_id, grandchild_4);
     tree.add_child(child_1_id, grandchild_5);
 
-    print_children_post_order(&tree, root_id);
-    println!("{}", tree.get(root_id).unwrap().data());
+    println!("Post-order:");
+    print_node_post_order(&tree, root_id);
+    println!("Pre-order:");
+    print_node_pre_order(&tree, root_id);
 }
 
-fn print_children_post_order(tree: &Tree<i32>, node_id: NodeId) {
+fn print_node_post_order(tree: &Tree<i32>, node_id: NodeId) {
     let node_ref: &Node<i32> = tree.get(node_id).unwrap();
 
     for child_id in node_ref.children() {
-        print_children_post_order(tree, *child_id);
-        println!("{}", tree.get(*child_id).unwrap().data());
+        print_node_post_order(tree, *child_id);
+    }
+
+    println!("{}", node_ref.data());
+}
+
+fn print_node_pre_order(tree: &Tree<i32>, node_id: NodeId) {
+    let node_ref: &Node<i32> = tree.get(node_id).unwrap();
+
+    println!("{}", node_ref.data());
+
+    for child_id in node_ref.children() {
+        print_node_pre_order(tree, *child_id);
     }
 }
