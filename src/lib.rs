1e317d6054fd5ec64e4e82f1b7ce38c0e1d5783b
Ian <iwburns8@gmail.com>
2016-09-28 22:48:57 -0500

    add documentation for id_tree itself and NodeId

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! A library for creating and modifying Tree structures.
+//!
+//! In this implementation, the `Tree` owns all of the `Node`s and all inter-`Node` relationships are
+//! managed with `NodeId`s.  This means that you will need a reference to the `Tree` to get a
+//! reference to any `Node`'s parent or child `Node`s.
+//!
+//! It is also important to note that this library does not support arbitrary Graph creation.  Any
+//! given Node can have **up to one parent** and **an arbitrary number of children**.
+//!
+
 extern crate snowflake;
 use self::snowflake::ProcessUniqueId;
 
@@ -8,6 +18,16 @@ pub use node::Node;
 pub use tree::TreeBuilder;
 pub use tree::Tree;
 
+///
+/// An identifier used to differentiate between Nodes within a Tree.
+///
+/// `NodeId`s are not something that you will ever have to worry about generating yourself.  `Tree`s
+/// generate `NodeId`s as you insert `Node`s into them.
+///
+/// `NodeId`s are also specific to the Tree that generated them.  This means that if you have two `Tree`s
+/// `A` and `B`, there's no worry of trying to access a Node in `A` with an identifier that came
+/// from `B`.
+///
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub struct NodeId {
     tree_id: ProcessUniqueId,
