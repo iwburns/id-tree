86ee262a45ea440f77d436bb12a49bf41642eed2
Ian Burns <iwburns8@gmail.com>
2016-10-06 09:39:06 -0500

    change most tree methods to using references to NodeIds
    this allows us to not require that the user clone NodeId's unless they have to.
    this also means that an extra copy of a NodeId will be easy to find and track because it was explicitly created.

--- examples/basic.rs
+++ examples/basic.rs
@@ -17,33 +17,33 @@ fn main() {
     let grandchild_5 = Node::new(5);
 
     let root_id: NodeId = tree.set_root(root_node);
-    let child_1_id: NodeId = tree.insert_with_parent(child_1, root_id.clone()).unwrap();
-    tree.insert_with_parent(child_3, root_id.clone()).unwrap();
-    tree.insert_with_parent(grandchild_4, child_1_id.clone()).unwrap();
-    tree.insert_with_parent(grandchild_5, child_1_id.clone()).unwrap();
+    let child_1_id: NodeId = tree.insert_with_parent(child_1, &root_id).unwrap();
+    tree.insert_with_parent(child_3, &root_id).unwrap();
+    tree.insert_with_parent(grandchild_4, &child_1_id).unwrap();
+    tree.insert_with_parent(grandchild_5, &child_1_id).unwrap();
 
     println!("Post-order:");
-    print_node_post_order(&tree, root_id.clone());
+    print_node_post_order(&tree, &root_id);
     println!("Pre-order:");
-    print_node_pre_order(&tree, root_id);
+    print_node_pre_order(&tree, &root_id);
 }
 
-fn print_node_post_order(tree: &Tree<i32>, node_id: NodeId) {
+fn print_node_post_order(tree: &Tree<i32>, node_id: &NodeId) {
     let node_ref: &Node<i32> = tree.get(node_id).unwrap();
 
     for child_id in node_ref.children() {
-        print_node_post_order(tree, child_id.clone());
+        print_node_post_order(tree, &child_id);
     }
 
     println!("{}", node_ref.data());
 }
 
-fn print_node_pre_order(tree: &Tree<i32>, node_id: NodeId) {
+fn print_node_pre_order(tree: &Tree<i32>, node_id: &NodeId) {
     let node_ref: &Node<i32> = tree.get(node_id).unwrap();
 
     println!("{}", node_ref.data());
 
     for child_id in node_ref.children() {
-        print_node_pre_order(tree, child_id.clone());
+        print_node_pre_order(tree, &child_id);
     }
 }
