5e4e41b6bc75d0a14510dee850ad34b2b279682c
Ian <iwburns8@gmail.com>
2017-11-16 23:32:23 -0600

    switch children to validate_node_id, add/fix tests

--- tests/error_tests.rs
+++ tests/error_tests.rs
@@ -538,37 +538,35 @@ fn ancestor_ids_old_id() {
     assert_eq!(ancestors.err().unwrap(), NodeIdNoLongerValid);
 }
 
-//#[test]
-//fn children_different_trees() {
-//    let mut a = VecTree::new();
-//    let b = VecTree::<i32>::new();
-//
-//    let root_id = a.insert(VecNode::new(1), AsRoot).unwrap();
-//
-//    // note usage of `b` instead of `a`
-//    let ancestors = b.children(&root_id);
-//
-//    assert!(ancestors.is_err());
-//    let error = ancestors.err().unwrap();
-//    assert_eq!(error, InvalidNodeIdForTree);
-//}
+#[test]
+fn children_different_trees() {
+    let mut a = VecTree::new();
+    let b = VecTree::<i32>::new();
 
-//#[test]
-//fn children_old_id() {
-//    let mut a = VecTree::new();
-//
-//    let root_id = a.insert(VecNode::new(1), AsRoot).unwrap();
-//    // `.clone()` required to get this error
-//    let root_id_clone = root_id.clone();
-//    let _ = a.remove(root_id, DropChildren).unwrap();
-//
-//    // note usage of cloned `NodeId`
-//    let ancestors = a.children(&root_id_clone);
-//
-//    assert!(ancestors.is_err());
-//    let error = ancestors.err().unwrap();
-//    assert_eq!(error, NodeIdNoLongerValid);
-//}
+    let root_id = a.insert(VecNode::new(1), AsRoot).unwrap();
+
+    // note usage of `b` instead of `a`
+    let ancestors = b.children(&root_id);
+
+    assert!(ancestors.is_err());
+    assert_eq!(ancestors.err().unwrap(), InvalidNodeIdForTree);
+}
+
+#[test]
+fn children_old_id() {
+    let mut a = VecTree::new();
+
+    let root_id = a.insert(VecNode::new(1), AsRoot).unwrap();
+    // `.clone()` required to get this error
+    let root_id_clone = root_id.clone();
+    a.remove(root_id, DropChildren).unwrap();
+
+    // note usage of cloned `NodeId`
+    let ancestors = a.children(&root_id_clone);
+
+    assert!(ancestors.is_err());
+    assert_eq!(ancestors.err().unwrap(), NodeIdNoLongerValid);
+}
 
 //#[test]
 //fn children_ids_different_trees() {
